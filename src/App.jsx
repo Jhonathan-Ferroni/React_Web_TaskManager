e9e9021b9755ce31ea5933916843594ba5a64786
Jhonathan-Ferroni <ferroni.jhonathan2403@gmail.com>
2026-03-30 19:27:47 -0300

    modelos iniciais de Interface

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,49 @@
 import { useState } from "react";
+import AddTask from "./components/AddTask";
+import Tasks from "./components/Tasks";
 
 function App() {
-  // State(Estado) - reações para usuário
-  const [message, setMessage] = useState("Olá mundo");
-  //Cada return só pode ter uma div
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Estudar programação",
+      description: "Estudar programação para se tornar um desenvolvedor júnior",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: "Fazer exercícios",
+      description: "Fazer exercícios para manter a saúde em dia",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      title: "Ler um livro",
+      description: "Ler um livro para expandir o conhecimento",
+      isCompleted: false,
+    },
+  ]);
+
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      //PRECISO ATUALIZAR ESSA TAREFA
+      if (task.id === taskId) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+      //NÃO PRECISO ATUALIZAR ESSA TAREFA
+      return task;
+    });
+    setTasks(newTasks);
+  }
   return (
-    <div>
-      <h1>{message}</h1>
-      <button
-        onClick={() => {
-          setMessage("Olá, fui clicado");
-        }}
-      >
-        Mudar mensagem
-      </button>
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6 ">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de tarefas
+        </h1>
+        <Tasks tasks={tasks} onTaskClick={onTaskClick} />
+        <AddTask />
+      </div>
     </div>
   );
 }
